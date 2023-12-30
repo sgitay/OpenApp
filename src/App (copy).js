4dd2bff972c7f750252ea3b984c8551691279ef3
sgitay <vitkaryogesh.jobs@gmail.com>
2023-12-30 22:50:23 +0530

    first push

---
+++ src/App (copy).js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <script type='text/javascript' language='JavaScript' src='https://in1.documents.adobe.com/public/embeddedWidget?wid=CBFCIBAA3AAABLblqZhBRG4EHKTvGdoOu4c6VSZP7V0H4M3TJu5QnsOv0dFrdpojgWJJA2twy_6SUmx9oWuY*'></script>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
